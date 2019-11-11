ac143834f21865c2959e8f31bc4cb266466825be
brandon moro <brandon_078@hotmail.com>
2019-11-11 21:51:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
-if speed != (40..60)
+if speed != (40..60).to_a
 	return true
 else
 	return false
@@ -9,5 +9,5 @@ end
 
 
 def not_safe?(speed)
-speed != (40..60) ? true : false
+speed != (40..60).to_a ? true : false
 end
